f8c1465740285f978252dcd5d56638f8e86834fb
Alexander Karlsson <alexander.karlsson2@elev.it-gymnasiet.se>
2013-01-14 10:56:49 +0100

    Fourth version
    Finished shoe class. Implemented shuffling and dealing, also a failsafe
    shuffle if you run out of cards.

--- Blackjack/Shoe.cs
+++ Blackjack/Shoe.cs
@@ -7,37 +7,37 @@ namespace Blackjack
 {
     public class Shoe
     {
-        private const int   CardsPerDeck = 52;
+        private const int CardsPerDeck = 52;
 
-        private Card[]  cards;
-        private int     numberOfDecks = 2;
-        private int     shoeLocation;
+        private Card[] cards;
+        private int numberOfDecks = 4;
+        private int shoeLocation;
 
-    public delegate void ShoeEventHandler(object sender, EventArgs e);
+        public delegate void ShoeEventHandler(object sender, EventArgs e);
 
-    public event ShoeEventHandler EndofShoeEvent;
-    public event ShoeEventHandler ShuffleEvent;
+        public event ShoeEventHandler EndofShoeEvent;
+        public event ShoeEventHandler ShuffleEvent;
 
-    public int NumberOfDecks 
-    { 
-        get
+        public int NumberOfDecks
         {
-            return numberOfDecks; 
+            get
+            {
+                return numberOfDecks;
+            }
+            set
+            {
+
+                if (numberOfDecks != value)
+                    numberOfDecks = value;
+            }
         }
-        set
+
+        public int ShoeLocation
         {
-            
-           if (numberOfDecks != value) 
-               numberOfDecks = value; 
+            get { return shoeLocation; }
         }
-    }
 
-    public int ShoeLocation 
-    { 
-        get{ return shoeLocation; }
-    }
-
-    public void Init()
+        public void Init()
     {
         shoeLocation = 0;
 
@@ -56,5 +56,73 @@ namespace Blackjack
         }
     }
 
-    
+        //End-of-Deck property
+        public bool EoD
+        {
+            get { return (shoeLocation >= cards.Length * .66); }
+        }
+
+        //Beginning-of-Deck property
+        public bool BoD
+        {
+            get { return (shoeLocation == 0); }
+        }
+
+        public void Shuffle()
+        {
+            Random rand = new Random(unchecked((int)DateTime.Now.Ticks));
+
+            for (int i = 0; i < rand.Next(10, 100); i++)
+            {
+                shoeLocation = 0;
+
+                Card[] shuffledCards = new Card[numberOfDecks * CardsPerDeck];
+
+                Random index = new Random(unchecked((int)DateTime.Now.Ticks));
+                int upperLimit = cards.GetUpperBound(0);
+
+                for (int j = 0; j < numberOfDecks * CardsPerDeck; j++)
+                {
+                    int k = index.Next(0, upperLimit);
+                    shuffledCards[j] = cards[k];
+                    cards[k] = cards[upperLimit--];
+                }
+
+                cards = shuffledCards;
+            }
+
+            ShuffleEvent(this, EventArgs.Empty);
+        }
+
+        //Indexer
+        public Card this[int index]
+        {
+            get
+            {
+                if (index < 0 || index > 52)
+                    throw new ArgumentOutOfRangeException();
+                else
+                    return cards[index];
+            }
+        }
+
+        public Card Next()
+        {
+            shoeLocation++;
+
+            if (shoeLocation < cards.GetUpperBound(0))
+            {
+                if(EoD)
+                    EndofShoeEvent(this, EventArgs.Empty);
+
+                return cards[shoeLocation];
+            }
+            else
+            {
+            //Shuffles cards of shoes, if cards run out
+                Shuffle();
+                return cards[shoeLocation];
+            }
+        }
+    }
 }
