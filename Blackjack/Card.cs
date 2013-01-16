d77f0d714e3db8db624ff3ace7d2e58fd586dffe
Alexander Karlsson <alexander.karlsson2@elev.it-gymnasiet.se>
2013-01-16 11:09:49 +0100

    Fifth version
    Finished card class. Started on hand class, there are some bugs which
    will be ironed out later.

--- Blackjack/Card.cs
+++ Blackjack/Card.cs
@@ -2,11 +2,59 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Resources;
+using System.Collections;
+using System.Globalization;
+using System.Reflection;
 using Nancy;
 
 namespace Blackjack
 {
-    public class Card
+    public class Card : ICloneable
     {
+        private CardType cardType;
+        private int value;
+        private int trueValue;
+
+        public enum CardType
+        {
+            Ace = 0,
+            Two = 1,
+            Three = 2,
+            Four = 3,
+            Five = 4,
+            Six = 5,
+            Seven = 6,
+            Eight = 7,
+            Nine = 8,
+            Ten = 9,
+            Jack = 10,
+            Queen = 11,
+            King = 12
+        }
+        public int Value
+        {
+            get { return value; }
+        }
+
+        public CardType FaceValue
+        {
+            get { return cardType; }
+        }
+
+        public int TrueValue
+        {
+            get { return trueValue; }
+        }
+
+        public Card Clone()
+        {
+            return new Card(cardType);
+        }
+
+        public static Card Clone(Card card)
+        {
+            return new Card(card.FaceValue);
+        }
     }
 }
